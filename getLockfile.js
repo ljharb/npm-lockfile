acc98439679afa2a33f8eccf873c2563ec36ca95
Jordan Harband <ljharb@gmail.com>
2019-02-21 16:46:11 -0800

    [Fix] fix logging messages for undefined date

--- getLockfile.js
+++ getLockfile.js
@@ -21,7 +21,7 @@ module.exports = function getLockfile(packageFile, date, logger = () => {}) {
 	const tmpDirP = getProjectTempDir();
 	const npmRC = path.join(path.dirname(packageFile), '.npmrc');
 	const copyPkg = tmpDirP.then(tmpDir => {
-		logger(chalk.blue(`Creating \`package.json\` in temp dir for ${date} lockfile`));
+		logger(chalk.blue(`Creating \`package.json\` in temp dir for ${date || '“now”'} lockfile`));
 		return Promise.all([
 			copyFile(packageFile, path.join(tmpDir, 'package.json')),
 			copyFile(npmRC, path.join(tmpDir, '.npmrc'))
@@ -29,7 +29,7 @@ module.exports = function getLockfile(packageFile, date, logger = () => {}) {
 	});
 	return Promise.all([tmpDirP, copyPkg]).then(([tmpDir]) => new Promise((resolve, reject) => {
 		const PATH = path.join(tmpDir, '../node_modules/.bin');
-		logger(chalk.blue(`Running npm install to create lockfile for ${date}...`));
+		logger(chalk.blue(`Running npm install to create lockfile for ${date || '“now”'}...`));
 		exec(`PATH=${PATH}:$PATH npm install --package-lock-only${date ? ` --before=${date}` : ''}`, { cwd: tmpDir }, err => {
 			if (err) {
 				reject(err);
@@ -38,7 +38,7 @@ module.exports = function getLockfile(packageFile, date, logger = () => {}) {
 			}
 		});
 	})).then(tmpDir => {
-		logger(chalk.blue(`Reading lockfile contents for ${date}...`));
+		logger(chalk.blue(`Reading lockfile contents for ${date || '“now”'}...`));
 		const lockfile = path.join(tmpDir, 'package-lock.json');
 		return readFile(lockfile, { encoding: 'utf-8' });
 	});
